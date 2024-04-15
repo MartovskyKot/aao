18b44f59abec317c584e85d2ba2f7cb0d5dc7588
Mila Bozorova <n0ragami@mail.ru>
2024-04-15 14:14:28 +0500

    мой первый коммит

---
+++ Seleniumtests_bozorova/SeleniumTestForPractice.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Seleniumtest_bozorova;
+
+public class SeleniumTestForPractice
+{
+    [Test]
+    public void Authorization()
+    {
+        var options = new ChromeOptions();
+        options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
+        
+        // -звйти в хром ( с помощью вебдрайвера)
+        var driver = new ChromeDriver(options);
+        
+        // -перейти по урлу https://staff-testing.testkontur.ru
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
+        Thread.Sleep(3000);
+       
+        //-ввести логин и пароль
+        var login = driver.FindElement(By.Id("Username"));
+        login.SendKeys("user");
+
+        var password = driver.FindElement(By.Name("Password"));
+        password.SendKeys("1q2w3e4r%T");
+        
+        Thread.Sleep(3000);
+
+        // -нажать на кнопку "войти"
+        var enter = driver.FindElement(By.Name("button"));
+        enter.Click();
+        
+        Thread.Sleep(3000);
+        
+        // - проверяем что находимся на нужной странице
+        var currentUrl = driver.Url;
+        Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news");
+        
+        
+        //- закрываем браузер и убиваем процесс драйвера
+        driver.Quit();
+    }
+}
